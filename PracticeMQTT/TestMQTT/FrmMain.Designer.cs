f03b4dddb09e4f6b72da302e796132e646e9d5f7
BlancBunny <nhsk0767@gmail.com>
2021-06-28 09:51:46 +0900

    branch commit

---
+++ PracticeMQTT/TestMQTT/FrmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace DeviceSubApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtMsg = new System.Windows.Forms.TextBox();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.LblAlert = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.label1.Location = new System.Drawing.Point(50, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Message";
+            // 
+            // TxtMsg
+            // 
+            this.TxtMsg.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.TxtMsg.Location = new System.Drawing.Point(163, 58);
+            this.TxtMsg.Name = "TxtMsg";
+            this.TxtMsg.Size = new System.Drawing.Size(376, 30);
+            this.TxtMsg.TabIndex = 3;
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.BtnConnect.Location = new System.Drawing.Point(154, 221);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(124, 29);
+            this.BtnConnect.TabIndex = 6;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.BtnDisconnect.Location = new System.Drawing.Point(414, 221);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(124, 29);
+            this.BtnDisconnect.TabIndex = 7;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.BtnSend.Location = new System.Drawing.Point(284, 221);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(124, 29);
+            this.BtnSend.TabIndex = 8;
+            this.BtnSend.Text = "Send";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // LblAlert
+            // 
+            this.LblAlert.AutoSize = true;
+            this.LblAlert.Font = new System.Drawing.Font("나눔고딕", 15F);
+            this.LblAlert.Location = new System.Drawing.Point(159, 100);
+            this.LblAlert.Name = "LblAlert";
+            this.LblAlert.Size = new System.Drawing.Size(0, 23);
+            this.LblAlert.TabIndex = 10;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 272);
+            this.Controls.Add(this.LblAlert);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.TxtMsg);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("나눔고딕", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "TestPublisher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtMsg;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Label LblAlert;
+    }
+}
+
---
+++ PracticeMQTT/TestMQTT/FrmMain.cs
@@ -0,0 +1,63 @@
+﻿using M2Mqtt;
+using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeviceSubApp
+{
+    public partial class FrmMain : Form
+    {
+        MqttClient client;
+        readonly string ip = "210.119.12.96"; // 서버 IP
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitializeAllData();
+        }
+
+        private void InitializeAllData()
+        {
+            // client 객체 생성
+            try
+            {
+                IPAddress brokerAddress = IPAddress.Parse(ip);
+                client = new MqttClient(brokerAddress);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            client.Connect("SUBSCR01");   // SUBSCR01
+
+            BtnConnect.Enabled = false;
+            BtnDisconnect.Enabled = true;
+
+            LblAlert.Text = "CONNECTED!!";
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
+            LblAlert.Text = "DISCONNECTED!!";
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            string subscriptionTopic = "TOMATO/";
+            // Publish
+            client.Publish(subscriptionTopic, // topic
+                              Encoding.UTF8.GetBytes(TxtMsg.Text), // message body
+                              0, // QoS level
+                              true); // retained
+            LblAlert.Text = "SUCCESS!!";
+        }
+    }
+}
