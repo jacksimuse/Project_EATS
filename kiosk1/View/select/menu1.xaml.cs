9ca2733b27158114a5f4fb12295d9a38b72f806e
tjdwn6459 <tjdwn6459@naver.com>
2021-08-03 17:30:19 +0900

    210803 메뉴창

--- kiosk1/View/select/menu1.xaml.cs
+++ kiosk1/View/select/menu1.xaml.cs
@@ -24,5 +24,15 @@ namespace kiosk1.View.select
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
