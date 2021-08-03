7212bff48e62dcbc18016bf266bb46bb637b6b24
tjdwn6459 <tjdwn6459@naver.com>
2021-08-03 14:27:07 +0900

    cxf

---
+++ kiosk1/View/main/mainView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kiosk1.View.main
+{
+    /// <summary>
+    /// mainView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class mainView : Page
+    {
+        public mainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
