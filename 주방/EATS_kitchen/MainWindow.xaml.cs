60db6f725d79e31653b5696ecaa6d7093a83afd5
jacksimuse <cjh930306@naver.com>
2021-08-03 14:10:21 +0900

    서빙로봇 운행상황 및 주방 UI DB연동전까지 완료

--- 주방/EATS_kitchen/MainWindow.xaml.cs
+++ 주방/EATS_kitchen/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EATS_kitchen
 {
@@ -48,6 +37,20 @@ namespace EATS_kitchen
             {
                 throw;
             }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            LblDatetime.Content = DateTime.Now.ToString("yy년MM월dd일 HH:mm:ss");
         }
     }
 }
