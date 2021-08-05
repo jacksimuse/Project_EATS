6ff7c4e0b7c40d387dc02af2a9d53c19c0edf367
jacksimuse <cjh930306@naver.com>
2021-08-05 10:04:56 +0900

    MQTT에서 Json으로 값을 받은 ordercode를 linq로 DB에서 메뉴이름을 가져오기

--- 주방/EATS_kitchen/Helper/Commons.cs
+++ 주방/EATS_kitchen/Helper/Commons.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using EATS_kitchen.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EATS_kitchen
 {
@@ -17,5 +21,17 @@ namespace EATS_kitchen
             return await ((MetroWindow)Application.Current.MainWindow)
                 .ShowMessageAsync(title, message, style, null);
         }
+
+        internal static List<OrderDetailtbl> GetDetail()
+        {
+            List<OrderDetailtbl> list;
+
+            using (var ctx = new EATSEntities())
+                list = ctx.OrderDetailtbl.ToList();
+
+            return list;
+        }
+
+
     }
 }
