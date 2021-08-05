f4553f81e515ac214a8f45846a2782ffeb96f66a
jacksimuse <cjh930306@naver.com>
2021-08-05 10:04:56 +0900

    MQTT에서 Json으로 값을 받은 ordercode를 linq로 DB에서 메뉴이름을 가져오기

--- 주방/EATS_kitchen/Order.xaml.cs
+++ 주방/EATS_kitchen/Order.xaml.cs
@@ -1,10 +1,20 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using Newtonsoft.Json;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Windows.Threading;
+using System.Linq;
 
 namespace EATS_kitchen
 {
@@ -13,10 +23,40 @@ namespace EATS_kitchen
     /// </summary>
     public partial class Order : Page
     {
+        MqttClient client;
+        string connectionString;
+        delegate void UpdateTextCallback(string message);
+        string thisId = "EATS_Subscrb";
+
         BackgroundWorker _worker = null;
-       
+
         bool isClicked = false;
         bool isUsing = false;
+
+
+        public Order()
+        {
+            InitializeComponent();
+
+            IPAddress brokerAddress = IPAddress.Parse("127.0.0.1");
+            client = new MqttClient(brokerAddress);
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            client.Connect(thisId);
+            client.Subscribe(new string[] { "EATS/order/data/" },
+                new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+            
+            MessageBox.Show("MQTT 서버에 연결");
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);
+            UpdateText(message);
+            var currentData = JsonConvert.DeserializeObject<Dictionary<string, string>>(message); // 데이터를 받을때 역 직렬화(DeserializeObject) <-> 보낼땐 직렬화(SerializeObject)
+           //var message2 =  $"테이블이름 : {currentData["table"].ToString()} \n주문내역 : {currentData["order"].ToString()}";
+            //UpdateText(message2);
+        }
+
         private async void Progressing()
         {
             if (isUsing)
@@ -41,11 +81,55 @@ namespace EATS_kitchen
             {
                 BtnDriving.Background = Brushes.Gold;
             }
-                        
         }
-        public Order()
+
+        private void UpdateText(string message)
         {
-            InitializeComponent();
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+            {
+                var currentData = JsonConvert.DeserializeObject<Dictionary<string, string>>(message); // 데이터를 받을때 역 직렬화(DeserializeObject) <-> 보낼땐 직렬화(SerializeObject)
+                DateTime dt = DateTime.Now;
+
+                switch (currentData["table"].ToString())
+                {
+                    case "1":
+                        GetOrderCode();
+                        TxbTbl1.Text = currentData["ordercode"].ToString();
+                        Lbltbl1.Content = "주문시각  " + dt.ToString("t");
+                        
+                        break;
+                    case "2":
+                        TxbTbl2.Text = currentData["ordercode"].ToString();
+                        Lbltbl2.Content = "주문시각  " + dt.ToString("t");
+
+                        break;
+                    case "3":
+                        TxbTbl3.Text = currentData["ordercode"].ToString();
+                        Lbltbl3.Content = "주문시각  " + dt.ToString("t");
+                        break;
+                    case "4":
+                        TxbTbl4.Text = currentData["ordercode"].ToString();
+                        Lbltbl4.Content = "주문시각  " + dt.ToString("t");
+
+                        break;
+                }
+            }));
+            
+        }
+
+        private void GetOrderCode()
+        {
+            // select MenuCode from OrderDetailtbl where OrderCode ='20210802001';
+            var menucodes = Commons.GetDetail().Where(o => o.OrderCode.Equals("20210802001"))
+                
+            if (menucodes == null)
+            {
+                Commons.ShowMessageAsync("오류", "값이 없습니다.");
+            }
+            else
+            {
+                Commons.ShowMessageAsync("찾았다", "없습니다.");
+            }
         }
 
         private void Btntbl1_Click(object sender, RoutedEventArgs e)
@@ -53,7 +137,7 @@ namespace EATS_kitchen
             Progressing();
             DrivingCheck();
         }
-        
+
         private void Btntbl2_Click(object sender, RoutedEventArgs e)
         {
             Progressing();
@@ -103,5 +187,11 @@ namespace EATS_kitchen
 
         }
 
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            client.Disconnect();
+        }
     }
 }
+
+
