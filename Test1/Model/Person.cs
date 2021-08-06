5589b0b41a1ccad38b824918602e1e274ae26c37
tjdwn6459 <tjdwn6459@naver.com>
2021-08-06 16:24:42 +0900

    210806

---
+++ Test1/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Model
+{
+    public class Person
+    {
+        private string nameValue;
+
+        public string Id { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ kiosk1/Model/Waitnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiosk1.Model
+{
+    public class Waitnum
+    {
+        public int num { get; set; }
+    }
+}
