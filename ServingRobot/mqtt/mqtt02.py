3dc8ec762bd32a03de4760bf20be95f1870a97d9
ghd0276 <ghd0276@naver.com>
2021-08-08 19:44:41 +0900

    8/8 라인트레이스 완료

--- ServingRobot/mqtt/mqtt02.py
+++ ServingRobot/mqtt/mqtt02.py
@@ -7,12 +7,18 @@ import os
 import time
 
 pin = 4        # 라인 근접 센서
-mpin1 = 20      # 앞 왼 바퀴1
-mpin2 = 21      # 앞 왼 바퀴2
-mpin3 = 6       # 앞 오 바퀴1
-mpin4 = 12      # 앞 오 바퀴2
-ena = 17        # 앞 왼 바퀴 enable 입력
-enb = 18        # 앞 오 바퀴 enable 입력 
+mpin1 = 24      # 앞 왼 바퀴1
+mpin2 = 25      # 앞 왼 바퀴2
+mpin3 = 5       # 앞 오 바퀴1
+mpin4 = 13      # 앞 오 바퀴2
+ena = 22        # 앞 오 바퀴 enable 입력 
+enb = 23        # 앞 오 바퀴 enable 입력 
+mpin5 = 20      # 뒤 왼 바퀴1
+mpin6 = 21      # 뒤 왼 바퀴2
+mpin7 = 6       # 뒤 오 바퀴1
+mpin8 = 12      # 뒤 오 바퀴2
+enc = 17        # 뒤 왼 바퀴 enable 입력
+end = 18        # 뒤 오 바퀴 enable 입력 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN)
@@ -22,34 +28,58 @@ GPIO.setup(mpin3, GPIO.OUT)
 GPIO.setup(mpin4, GPIO.OUT)
 GPIO.setup(ena, GPIO.OUT)
 GPIO.setup(enb, GPIO.OUT)
+GPIO.setup(mpin5, GPIO.OUT)
+GPIO.setup(mpin6, GPIO.OUT)
+GPIO.setup(mpin7, GPIO.OUT)
+GPIO.setup(mpin8, GPIO.OUT)
+GPIO.setup(enc, GPIO.OUT)
+GPIO.setup(end, GPIO.OUT)
 
 pa = GPIO.PWM(ena, 100)
 pb = GPIO.PWM(enb, 100)
 pa.start(100)
 pb.start(100)
+pc = GPIO.PWM(enc, 100)
+pd = GPIO.PWM(end, 100)
+pc.start(100)
+pd.start(100)
 
 GPIO.output(mpin1, False)
 GPIO.output(mpin2, False)
 GPIO.output(mpin3, False)
 GPIO.output(mpin4, False)
+GPIO.output(mpin5, False)
+GPIO.output(mpin6, False)
+GPIO.output(mpin7, False)
+GPIO.output(mpin8, False)
 
 def setOg():
     pa.ChangeDutyCycle(100)
     pb.ChangeDutyCycle(100)
+    pc.ChangeDutyCycle(100)
+    pd.ChangeDutyCycle(100)
 
 def set_left():
-    pa.ChangeDutyCycle(40)
-    GPIO.output(mpin1, True)
+    pc.ChangeDutyCycle(40)
+    GPIO.output(mpin1, False)
     GPIO.output(mpin2, False)
     GPIO.output(mpin3, False)
     GPIO.output(mpin4, True)
+    GPIO.output(mpin5, True)
+    GPIO.output(mpin6, False)
+    GPIO.output(mpin7, True)
+    GPIO.output(mpin8, False)
 
 def set_right():
-    pb.ChangeDutyCycle(40)
+    pd.ChangeDutyCycle(40)
     GPIO.output(mpin1, False)
     GPIO.output(mpin2, True)
-    GPIO.output(mpin3, True)
+    GPIO.output(mpin3, False)
     GPIO.output(mpin4, False)
+    GPIO.output(mpin5, False)
+    GPIO.output(mpin6, True)
+    GPIO.output(mpin7, False)
+    GPIO.output(mpin8, True)
 
 def set_start():
     setOg()
@@ -57,6 +87,10 @@ def set_start():
     GPIO.output(mpin2, True)
     GPIO.output(mpin3, False)
     GPIO.output(mpin4, True)
+    GPIO.output(mpin5, False)
+    GPIO.output(mpin6, True)
+    GPIO.output(mpin7, True)
+    GPIO.output(mpin8, False)
 
 def set_back():
     setOg()
@@ -64,6 +98,10 @@ def set_back():
     GPIO.output(mpin2, False)
     GPIO.output(mpin3, True)
     GPIO.output(mpin4, False)
+    GPIO.output(mpin5, True)
+    GPIO.output(mpin6, False)
+    GPIO.output(mpin7, False)
+    GPIO.output(mpin8, True)
 
 def stop():
     setOg()
@@ -71,6 +109,10 @@ def stop():
     GPIO.output(mpin2, False)
     GPIO.output(mpin3, False)
     GPIO.output(mpin4, False)
+    GPIO.output(mpin5, False)
+    GPIO.output(mpin6, False)
+    GPIO.output(mpin7, False)
+    GPIO.output(mpin8, False)
 
 def on_message(client, userdata, message):
     topic=str(message.topic)
