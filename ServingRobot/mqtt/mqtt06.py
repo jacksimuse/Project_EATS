5b7b1a94e723f28914410cfb945aa2ed3f123763
ghd0276 <ghd0276@naver.com>
2021-08-10 12:20:04 +0900

    8/10 초음파 센서 추가

--- ServingRobot/mqtt/mqtt06.py
+++ ServingRobot/mqtt/mqtt06.py
@@ -1,3 +1,4 @@
+from platform import dist
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import sys
@@ -13,6 +14,7 @@ event_pause.clear()
 
 pin = 19        # 라인 근접 센서
 pin2 = 4        # 라인 근접 센서
+
 mpin1 = 24      # 앞 왼 바퀴1
 mpin2 = 25      # 앞 왼 바퀴2
 mpin3 = 5       # 앞 오 바퀴1
@@ -24,7 +26,11 @@ mpin6 = 21      # 뒤 왼 바퀴2
 mpin7 = 6       # 뒤 오 바퀴1
 mpin8 = 12      # 뒤 오 바퀴2
 enc = 17        # 뒤 왼 바퀴 enable 입력
-end = 18        # 뒤 오 바퀴 enable 입력 
+end = 18        # 뒤 오 바퀴 enable 입력
+
+triggerPin = 16 # 초음파 트리거핀
+echoPin = 26    # 초음파 에코핀
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN)
@@ -41,6 +47,8 @@ GPIO.setup(mpin7, GPIO.OUT)
 GPIO.setup(mpin8, GPIO.OUT)
 GPIO.setup(enc, GPIO.OUT)
 GPIO.setup(end, GPIO.OUT)
+GPIO.setup(triggerPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
 
 pa = GPIO.PWM(ena, 100)
 pb = GPIO.PWM(enb, 100)
@@ -51,26 +59,48 @@ pd = GPIO.PWM(end, 100)
 pc.start(40)
 pd.start(40)
 
-GPIO.output(mpin1, False)
-GPIO.output(mpin2, False)
-GPIO.output(mpin3, False)
-GPIO.output(mpin4, False)
-GPIO.output(mpin5, False)
-GPIO.output(mpin6, False)
-GPIO.output(mpin7, False)
-GPIO.output(mpin8, False)
+# GPIO.output(mpin1, False)
+# GPIO.output(mpin2, False)
+# GPIO.output(mpin3, False)
+# GPIO.output(mpin4, False)
+# GPIO.output(mpin5, False)
+# GPIO.output(mpin6, False)
+# GPIO.output(mpin7, False)
+# GPIO.output(mpin8, False)
+
+h = 100
+r = 40
+sec = 0.00001
+
+def ultra():
+    while True:
+        GPIO.output(triggerPin, GPIO.LOW)
+        time.sleep(0.00001)
+        GPIO.output(triggerPin, GPIO.HIGH)
+
+        while GPIO.input(echoPin) == 0: #초음파 전송이 끝나는 전송시간
+            start = time.time()         #을 저장
+        while GPIO.input(echoPin) == 1: #초음파 수신이 완료된 때 시간
+            stop = time.time()          #을 저장
+
+        rtTotime = stop - start
+        distance = rtTotime * 34000 / 2
+        print("distance : %.2fcm" % distance)
+
+        time.sleep(0.1)
+        return distance
 
 def setOg():
-    pa.ChangeDutyCycle(40)
-    pb.ChangeDutyCycle(40)
-    pc.ChangeDutyCycle(40)
-    pd.ChangeDutyCycle(40)
+    pa.ChangeDutyCycle(r)
+    pb.ChangeDutyCycle(r)
+    pc.ChangeDutyCycle(r)
+    pd.ChangeDutyCycle(r)
 
 def set_left():
-    pa.ChangeDutyCycle(100)
-    pb.ChangeDutyCycle(100)
-    pd.ChangeDutyCycle(100)
-    pc.ChangeDutyCycle(100)
+    pa.ChangeDutyCycle(h)
+    pb.ChangeDutyCycle(h)
+    pd.ChangeDutyCycle(h)
+    pc.ChangeDutyCycle(h)
     GPIO.output(mpin1, True)
     GPIO.output(mpin2, False)
     GPIO.output(mpin3, False)
@@ -81,10 +111,10 @@ def set_left():
     GPIO.output(mpin8, False)
 
 def set_right():
-    pa.ChangeDutyCycle(100)
-    pb.ChangeDutyCycle(100)
-    pc.ChangeDutyCycle(100)
-    pd.ChangeDutyCycle(100)
+    pa.ChangeDutyCycle(h)
+    pb.ChangeDutyCycle(h)
+    pc.ChangeDutyCycle(h)
+    pd.ChangeDutyCycle(h)
     GPIO.output(mpin1, False)
     GPIO.output(mpin2, True)
     GPIO.output(mpin3, True)
@@ -105,17 +135,13 @@ def set_start():
         #         if event_pause.is_set() == False:
         #             break
         #         time.sleep(1)
-        if (GPIO.input(pin) == False) and (GPIO.input(pin2) == False):
+        distance = ultra()
+        if distance < 60:
+            stop()
+        elif (GPIO.input(pin) == False) and (GPIO.input(pin2) == False):
             #print("no path")
-            setOg()
-            GPIO.output(mpin1, False)
-            GPIO.output(mpin2, False)
-            GPIO.output(mpin3, False)
-            GPIO.output(mpin4, False)
-            GPIO.output(mpin5, False)
-            GPIO.output(mpin6, False)
-            GPIO.output(mpin7, False)
-            GPIO.output(mpin8, False)
+            #setOg()
+            stop()
             #time.sleep(0.00001)
             break
         elif (GPIO.input(pin) == True) and (GPIO.input(pin2) == True):
@@ -132,34 +158,13 @@ def set_start():
             time.sleep(0.00001)
         elif (GPIO.input(pin) == False) and (GPIO.input(pin2) == True):
             #print("right")
-            pa.ChangeDutyCycle(100)
-            pb.ChangeDutyCycle(100)
-            pc.ChangeDutyCycle(100)
-            pd.ChangeDutyCycle(100)
-            GPIO.output(mpin1, False)
-            GPIO.output(mpin2, True)
-            GPIO.output(mpin3, True)
-            GPIO.output(mpin4, False)
-            GPIO.output(mpin5, False)
-            GPIO.output(mpin6, True)
-            GPIO.output(mpin7, False)
-            GPIO.output(mpin8, True)
-            time.sleep(0.07)
+            set_right()
+            time.sleep(sec)
         elif (GPIO.input(pin) == True) and (GPIO.input(pin2) == False):
             #print("left")
-            pa.ChangeDutyCycle(100)
-            pb.ChangeDutyCycle(100)
-            pd.ChangeDutyCycle(100)
-            pc.ChangeDutyCycle(100)
-            GPIO.output(mpin1, True)
-            GPIO.output(mpin2, False)
-            GPIO.output(mpin3, False)
-            GPIO.output(mpin4, True)
-            GPIO.output(mpin5, True)
-            GPIO.output(mpin6, False)
-            GPIO.output(mpin7, True)
-            GPIO.output(mpin8, False)
-            time.sleep(0.07)
+            set_left()
+            time.sleep(sec)
+        
 
 def set_position():
     while True:
@@ -169,24 +174,12 @@ def set_position():
         if (GPIO.input(pin) == True) and (GPIO.input(pin2) == True):
             break
         elif (GPIO.input(pin) == False) or (GPIO.input(pin2) == False):
-            pa.ChangeDutyCycle(100)
-            pb.ChangeDutyCycle(100)
-            pd.ChangeDutyCycle(100)
-            pc.ChangeDutyCycle(100)
-            GPIO.output(mpin1, True)
-            GPIO.output(mpin2, False)
-            GPIO.output(mpin3, False)
-            GPIO.output(mpin4, True)
-            GPIO.output(mpin5, True)
-            GPIO.output(mpin6, False)
-            GPIO.output(mpin7, True)
-            GPIO.output(mpin8, False)
+            set_left()
             time.sleep(0.001)
     stop()
 
 def stop():
     #event_stop.set()
-    setOg()
     GPIO.output(mpin1, False)
     GPIO.output(mpin2, False)
     GPIO.output(mpin3, False)
