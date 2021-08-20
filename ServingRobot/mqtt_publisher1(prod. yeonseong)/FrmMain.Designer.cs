5255051dd0a8082ecf4549e8151798683452b29c
ghd0276 <ghd0276@naver.com>
2021-08-20 15:21:25 +0900

    8/20 test

---
+++ ServingRobot/mqtt_publisher1(prod. yeonseong)/FrmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace DeviceSubApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.BtnSend1 = new System.Windows.Forms.Button();
+            this.LblAlert = new System.Windows.Forms.Label();
+            this.BtnSend3 = new System.Windows.Forms.Button();
+            this.BtnBack = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnConnect.Location = new System.Drawing.Point(151, 231);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(124, 29);
+            this.BtnConnect.TabIndex = 6;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnDisconnect.Location = new System.Drawing.Point(281, 231);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(124, 29);
+            this.BtnDisconnect.TabIndex = 7;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // BtnSend1
+            // 
+            this.BtnSend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnSend1.Location = new System.Drawing.Point(133, 54);
+            this.BtnSend1.Name = "BtnSend1";
+            this.BtnSend1.Size = new System.Drawing.Size(142, 47);
+            this.BtnSend1.TabIndex = 8;
+            this.BtnSend1.Text = "1";
+            this.BtnSend1.UseVisualStyleBackColor = true;
+            this.BtnSend1.Click += new System.EventHandler(this.BtnSend1_Click);
+            // 
+            // LblAlert
+            // 
+            this.LblAlert.AutoSize = true;
+            this.LblAlert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.LblAlert.Location = new System.Drawing.Point(236, 9);
+            this.LblAlert.Name = "LblAlert";
+            this.LblAlert.Size = new System.Drawing.Size(92, 25);
+            this.LblAlert.TabIndex = 10;
+            this.LblAlert.Text = "message";
+            // 
+            // BtnSend3
+            // 
+            this.BtnSend3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnSend3.Location = new System.Drawing.Point(291, 118);
+            this.BtnSend3.Name = "BtnSend3";
+            this.BtnSend3.Size = new System.Drawing.Size(142, 47);
+            this.BtnSend3.TabIndex = 12;
+            this.BtnSend3.Text = "4";
+            this.BtnSend3.UseVisualStyleBackColor = true;
+            this.BtnSend3.Click += new System.EventHandler(this.BtnSend3_Click);
+            // 
+            // BtnBack
+            // 
+            this.BtnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnBack.Location = new System.Drawing.Point(291, 178);
+            this.BtnBack.Name = "BtnBack";
+            this.BtnBack.Size = new System.Drawing.Size(142, 47);
+            this.BtnBack.TabIndex = 13;
+            this.BtnBack.Text = "Back";
+            this.BtnBack.UseVisualStyleBackColor = true;
+            this.BtnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnStart.Location = new System.Drawing.Point(291, 54);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(142, 47);
+            this.BtnStart.TabIndex = 14;
+            this.BtnStart.Text = "2";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.BtnStop.Location = new System.Drawing.Point(133, 178);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(142, 47);
+            this.BtnStop.TabIndex = 15;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(133, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 47);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 272);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnBack);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnSend3);
+            this.Controls.Add(this.LblAlert);
+            this.Controls.Add(this.BtnSend1);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.BtnConnect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "TestPublisher";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnSend1;
+        private System.Windows.Forms.Label LblAlert;
+        private System.Windows.Forms.Button BtnSend3;
+        private System.Windows.Forms.Button BtnBack;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ServingRobot/mqtt_publisher1(prod. yeonseong)/FrmMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace DeviceSubApp
+{
+    public partial class FrmMain : Form
+    {
+        MqttClient client;
+        string subscriptionTopic = "MOTOR/TEST/";
+        //string subscriptionTopic = "TRAFFICLIGHT/TEST/";
+        string message = string.Empty;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitializeAllData();
+        }
+
+        private void InitializeAllData()
+        {
+            IPAddress brokerAddress = IPAddress.Parse("210.119.12.93");
+            BtnStop.Enabled = false;
+            BtnBack.Enabled = false;
+            BtnSend1.Enabled = false;
+            BtnSend3.Enabled = false;
+            button1.Enabled = false;
+            BtnStart.Enabled = false;
+            BtnDisconnect.Enabled = false;
+
+            // client 연결
+            try
+            {
+                client = new MqttClient(brokerAddress);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            client.Connect("SUBSCR01");   // SUBSCR01
+
+            BtnConnect.Enabled = false;
+            BtnDisconnect.Enabled = true;
+            BtnSend1.Enabled = true;
+            BtnSend3.Enabled = true;
+            BtnStart.Enabled = true;
+            button1.Enabled = true;
+            BtnStop.Enabled = true;
+            BtnBack.Enabled = true;
+
+            LblAlert.Text = "CONNECTED!!";
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
+            LblAlert.Text = "DISCONNECTED!!";
+        }
+
+        private void BtnSend1_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            //BtnSend1.Enabled = false;
+            message = "1";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "1번테이블";
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            //BtnStart.Enabled = false;
+
+            message = "2";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "2번테이블";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            //BtnStart.Enabled = false;
+
+            message = "3";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "3번테이블";
+        }
+
+        private void BtnSend3_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            //BtnSend3.Enabled = false;
+            message = "4";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "4번테이블";
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            /*BtnStop.Enabled = false;
+            BtnPause.Enabled = false;
+
+            BtnStart.Enabled = true;
+            BtnSend1.Enabled = true;
+            BtnSend3.Enabled = true;*/
+
+            message = "t";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "Stop";
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            Reconnect();
+            message = "b";
+            // Publish
+            Publish(message);
+            LblAlert.Text = "Back";
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            if (client.IsConnected)
+                client.Disconnect();
+        }
+
+        private void Reconnect()
+        {
+            if (!client.IsConnected)
+            {
+                client.Connect("SUBSCR1");
+            }
+        }
+
+        private void Publish(string message)
+        {
+            client.Publish(subscriptionTopic, // topic
+                              Encoding.UTF8.GetBytes(message), // message body
+                              0, // QoS level
+                              true); // retained
+        }
+    }
+}
